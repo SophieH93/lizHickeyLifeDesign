aabbc55b33f6a68daca492d01ae10f2d11feb816
Sophie <sophiehickey123@gmail.com>
2020-09-07 20:45:19 +0000

    added context to render in views.py in courses app

--- courses/views.py
+++ courses/views.py
@@ -5,11 +5,10 @@ from django.conf import settings
 
 
 def courses(request):
-
     
     context = {
         'api_key': settings.GOOGLE_MAP_API_KEY,
     }
 
-    return render(request, 'courses/courses.html')
+    return render(request, 'courses/courses.html', context)
 
