fe012bce1462be5ad72422a1ab994697bbb245ef
Sophie <sophiehickey123@gmail.com>
2020-09-07 17:30:11 +0000

    added google map api_key to courses views.py

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 
 
 def courses(request):
 
+    
+    context = {
+        'api_key': settings.GOOGLE_MAP_API_KEY,
+    }
+
     return render(request, 'courses/courses.html')
+
