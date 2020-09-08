938a8659a5e3bc37e2d2f88fffcb7f85d10f1be9
Sophie <sophiehickey123@gmail.com>
2020-09-08 11:31:55 +0000

    added newManager class to blog models to displau published blogs

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
 
+    class NewManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset() .filter(status='published')
+
     options = (
     ('draft', 'Draft'),
     ('published', 'Published'),
@@ -16,6 +20,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     status = models.CharField(max_length=10,  choices=options, default='draft')
+    onjects = models.Manager()
+    newmanager = NewManager()
 
     class Meta:
         ordering = ('-publish',)
