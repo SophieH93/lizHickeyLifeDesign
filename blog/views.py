c734eedda9217f719fb71e600f9ff5e98810bf87
Sophie <sophiehickey123@gmail.com>
2020-09-08 11:35:51 +0000

    created view for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def blog(request):
+
+    all_posts = Post.newmanager.all()
+
+    return render(request, 'blog/blog.html', {'posts': all_posts})
