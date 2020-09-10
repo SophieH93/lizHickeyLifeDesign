9b46d80c28f89b30d8091acd707f36e23f4a85b4
Sophie <sophiehickey123@gmail.com>
2020-09-10 11:15:59 +0000

    added course model to courses view.py to fetch course from database

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.conf import settings
+from .models import Course
 
 # Create your views here.
 
 
 def courses(request):
-    
+    """
+    A view to display all courses & google map api
+    """
+    courses = Course.objects.all()
+
     context = {
+        'courses': courses,
         'api_key': settings.GOOGLE_MAP_API_KEY,
     }
 
