2b3c80ecef00fd1a564c47d35bd474c65aac74b2
Sophie <sophiehickey123@gmail.com>
2020-09-10 14:30:07 +0000

    added add to cart functionality

--- shopping_cart/contexts.py
+++ shopping_cart/contexts.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from courses.models import Course
 
 
@@ -6,6 +7,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     course_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        course = get_object_or_404(Course, pk=item_id)
+        total += quantity * course.price
+        course_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'course': course,
+        })
 
     context = {
         'cart_items' : cart_items,
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ from django.shortcuts import render
 def shopping_cart(request):
 
     return render(request, 'shopping_cart/shopping_cart.html')
+
+
+def add_to_cart(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
+
