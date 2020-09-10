c4cf26245d327c81d3f903422a1122b18615b454
Sophie <sophiehickey123@gmail.com>
2020-09-10 14:45:35 +0000

    amended course_count to course_total in context.py in cart app

--- shopping_cart/contexts.py
+++ shopping_cart/contexts.py
@@ -6,13 +6,13 @@ def cart_contents(request):
 
     cart_items = []
     total = 0
-    course_count = 0
+    course_total = 0
     cart = request.session.get('cart', {})
 
     for item_id, quantity in cart.items():
         course = get_object_or_404(Course, pk=item_id)
         total += quantity * course.price
-        course_count += quantity
+        course_total += quantity
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
@@ -22,7 +22,7 @@ def cart_contents(request):
     context = {
         'cart_items' : cart_items,
         'total': total,
-        'course_count' : course_count,
+        'course_total' : course_total,
     }
 
     return context
