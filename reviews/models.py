135c7cb0205325ae83bf0fbd5a80e16d658753cf
Sophie <sophiehickey123@gmail.com>
2020-09-12 13:55:08 +0000

    created review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from courses.models import Course
 
-# Create your models here.
+
+class Reviews(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)    
+    review = models.TextField(null=False, blank=False)
+    RATING_CHOICES = (
+        (0, 'No rating'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+    rate = models.IntegerField(choices=RATING_CHOICES, default=0)
+    
+    def __str__(self):
+        return self.review
