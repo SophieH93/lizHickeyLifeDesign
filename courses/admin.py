e58433ad6c3575f8d74263dc5fd078b8682d9d6d
Sophie <sophiehickey123@gmail.com>
2020-09-13 12:44:05 +0000

    Add review in admin in courses app

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Review
 
-# Register your models here.
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
@@ -12,4 +12,9 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
+class ReviewAdmin(admin.ModelAdmin):
+    readonly_fields = ('course', 'review')
+
+
 admin.site.register(Course, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
