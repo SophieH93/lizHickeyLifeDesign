9c4417ff88fb1b52a48c7f0488f10565ab119dcc
Sophie <sophiehickey123@gmail.com>
2020-09-14 22:20:21 +0000

    added success toast & styled toast

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.contrib import messages
+from courses.models import Course
 
 
 def shopping_cart(request):
@@ -9,7 +9,8 @@ def shopping_cart(request):
 
 
 def add_to_cart(request, item_id):
-
+    
+    course = Course.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -18,7 +19,20 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {course.name} to cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
 
+
+def remove_from_cart(request, item_id):
+    try:
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
