97abfc165fa2081663fc56e38d5237791814fc63
Sophie <sophiehickey123@gmail.com>
2020-09-15 12:31:16 +0000

    added media processor to settings.py

--- liz_hickey_lifedesign/settings.py
+++ liz_hickey_lifedesign/settings.py
@@ -92,8 +92,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'shopping_cart.contexts.cart_contents',
-                
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
