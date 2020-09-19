e5d14528cd09119932b2cc2dc36108bbf279a407
Sophie <sophiehickey123@gmail.com>
2020-09-19 11:13:51 +0000

    added edit course functionality for superuser

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.courses, name='courses'),
     path('<int:course_id>/', views.course_detail, name='course_detail'),
     path('add/', views.add_course, name='add_course'),
+    path('edit/<int:course_id>/', views.edit_course, name='edit_course'),
     path('<course_id>', views.add_review, name='add_review'),
 ]
