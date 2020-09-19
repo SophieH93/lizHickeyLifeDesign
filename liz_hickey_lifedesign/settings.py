650aa7a9bc9818e7a03ff917bf7820aab67772ff
Sophie <sophiehickey123@gmail.com>
2020-09-19 14:35:04 +0000

    set debug back to true

--- liz_hickey_lifedesign/settings.py
+++ liz_hickey_lifedesign/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True # 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['celtic-queen-lizzie.herokuapp.com', 'localhost']
 
@@ -186,7 +186,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
