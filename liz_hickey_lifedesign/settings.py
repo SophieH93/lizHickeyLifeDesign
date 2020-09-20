a79ab2ce9750f46fb3bf2f7fc71439de7bf6f164
Sophie <sophiehickey123@gmail.com>
2020-09-20 14:21:18 +0000

    added AWS_S3_OBJECT_PARAMETERS to settings.py

--- liz_hickey_lifedesign/settings.py
+++ liz_hickey_lifedesign/settings.py
@@ -199,6 +199,13 @@ GOOGLE_MAP_API_KEY = os.environ.get('GOOGLE_MAP_KEY')
 
 
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'celtic-queen-lizzie'
     AWS_S3_REGION_NAME = 'eu-west-2'
