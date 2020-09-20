e8237d84cccb31549107304af5293872ef6b767f
Sophie <sophiehickey123@gmail.com>
2020-09-20 17:33:56 +0000

    added static root to settings.py

--- liz_hickey_lifedesign/settings.py
+++ liz_hickey_lifedesign/settings.py
@@ -79,7 +79,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'liz_hickey_lifedesign.urls'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'  
 
 TEMPLATES = [
     {
@@ -186,6 +186,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
