d5010aea798d029de6465966ab67b7aafa0c7079
Sophie <sophiehickey123@gmail.com>
2020-09-27 12:40:12 +0000

    changed index name to home

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
+def home(request):
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/home.html')
