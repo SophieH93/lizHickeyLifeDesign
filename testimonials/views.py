2d639e8478a55debf368c4ac6d35099ae598bf52
Sophie <sophiehickey123@gmail.com>
2020-09-27 21:42:00 +0000

    added google maps api to testimonials views

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def testimonials(request):
 
-    return render(request, 'testimonials/testimonials.html')
+    context = {
+        'api_key': settings.GOOGLE_MAP_API_KEY,
+    }
+
+    return render(request, 'testimonials/testimonials.html', context)
 
 
 
