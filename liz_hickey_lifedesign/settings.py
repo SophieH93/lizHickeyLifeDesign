3db9889241b544703d1a1a1f2bb061dc83b74509
Sophie <sophiehickey123@gmail.com>
2020-09-29 19:28:33 +0000

    renamed app

--- liz_hickey_lifedesign/settings.py
+++ liz_hickey_lifedesign/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['celtic-queen-lizzie.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['lizhickeylifedesign.herokuapp.com', 'localhost']
 
 
 # Application definition
