1ea592aa98120f214eeba6f78a7a2664c2b61586
Sophie <sophiehickey123@gmail.com>
2020-09-30 21:01:49 +0000

    amended allowed hosts

--- liz_hickey_lifedesign/settings.py
+++ liz_hickey_lifedesign/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lizhickeylifedesign.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['lizhickeylifedesign.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
